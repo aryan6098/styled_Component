78406b389e67bff3f56024c3f1b553b7dc96e8d2
Aryan Raj <araj60988@gmail.com>
2022-09-11 21:08:07 +0530

    used props in styled components

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -10,12 +10,14 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${props => (props.invalid ? 'red':'black')}
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    background: ${props => (props.invalid ? 'ffd7d7' : 'transparent')}
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -27,14 +29,6 @@ const FormControl = styled.div`
     border-color: #8b005d;
   }
 
-  &.invalid input {
-    border-color: red;
-    background-color: #ffd7d7;
-  }
-
-  &.invalid label {
-    color: red;
-  }
 `;
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -57,7 +51,7 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValid && 'invalid'}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
